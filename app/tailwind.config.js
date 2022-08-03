00ca054202d1e9a7a7e2a37b827b2b01aa664586
João Ribeiro Bezerra <joaorb64@gmail.com>
2022-08-03 17:31:48 +0100

    A

---
+++ app/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito_sans: ['Nunito Sans', 'sans-serif']
+      }
+    },
+    colors: {
+      very_dark_blue: 'hsl(200, 15%, 8%)',
+      dark_gray: 'hsl(0, 0%, 52%)',
+      very_light_gray: 'hsl(0, 0%, 98%)',
+      white: 'hsl(0, 0%, 100%)'
+    }
+  },
+  plugins: []
+}
